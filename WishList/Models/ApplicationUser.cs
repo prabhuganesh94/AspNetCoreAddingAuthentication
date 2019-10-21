599500ac7e0432fd6ee70084930f132578e8181f
Natarajan, Ganeshprabhu <gnataraj@tmp.com>
2019-10-21 16:23:27 +0530

    added items property name

--- WishList/Models/ApplicationUser.cs
+++ WishList/Models/ApplicationUser.cs
@@ -5,6 +5,7 @@ namespace WishList.Models
 {
     public class ApplicationUser : IdentityUser
     {
-        public virtual ICollection<Item> Item { get; set; }
+        public virtual ICollection<Item> Items { get; set; }
     }
 }
+`
